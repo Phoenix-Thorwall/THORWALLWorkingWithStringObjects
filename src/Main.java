784c42bf306181131984a6d2124b8044733851ef
student <sample@bths.edu>
2022-10-07 12:26:49 -0400

    Need To Finish @ L8er

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+    String str1 = "Hello";
+    String str2 = new String("I HATE YOU");
+    String s = "My partner Sonam thinks I am hillarious";
+    String str3 = new String(s);
+}

fcc6a6446e532e0aad7274460f6f99349314b0d9
Phoenix <phoenix.thorwall@gmail.com>
2022-10-08 16:51:16 -0400

    I Finished The Lab!!!!

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -2,6 +2,11 @@ public class LabRunner
 {
     public static void main(String[]args)
     {
-        System.out.println("");
+        BabyParrot baby = new BabyParrot("Toucan", 3, 4.25);
+        baby.setName("Lil' Beans");
+        String info = baby.parrotInfo(); // store returned string in variable
+        System.out.println(info);        // print the returned string
+
+
     }
 }
